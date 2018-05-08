52e3b78660f72d3b7e2ee13e117077e66e874419
Mariusz <info@mbachurski.com>
2018-05-07 23:38:11 -0700

    Adding components

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/header'
-import './index.css'
+import Welcome from '../components/welcome'
+import '../styles/styles.css'
 
 const Layout = ({ children, data }) => (
   <div>
@@ -14,17 +15,9 @@ const Layout = ({ children, data }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+    <Welcome />
+
   </div>
 )
 
