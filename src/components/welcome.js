928860324ea6e3596b5599905d9eb4c4d6d41a26
Mariusz <info@mbachurski.com>
2018-05-07 23:39:35 -0700

    Basic skeleton of component

---
+++ src/components/welcome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import Hero from '../assets/images/hero_cafe.jpg'
+
+const Welcome = () => (
+  <div className="section__welcome">
+    <h1 className="welcome__header">Juantonne Café</h1>
+
+  </div>
+)
+
+export default Welcome
