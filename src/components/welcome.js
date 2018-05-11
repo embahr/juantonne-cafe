0a695456dc773b0269da135b4a84f51b066d8b27
Mariusz <info@mbachurski.com>
2018-05-11 12:36:09 -0700

    Fixing some layout issues

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Hero from '../assets/images/hero_cafe.jpg'
+import Hero_m from '../assets/images/hero_mobile.jpg'
 import Coffee from '../components/IconCoffee'
 
 const Welcome = ({ heading, subHeading }) => (
   <div id="welcome" className="section__welcome">
+    <div className="welcome__backdrop"></div>
     <div className="welcome__header">
       <h1>{ heading }</h1>
       <h3>{ subHeading }</h3>
